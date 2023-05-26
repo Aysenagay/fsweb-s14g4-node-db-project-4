602b69dde5743dd7bd3da6190d9322bbff43356b
Aysenagay <101216489+Aysenagay@users.noreply.github.com>
2023-05-26 12:00:09 +0300

    proje tamamlandı.

--- api/server.js
+++ api/server.js
@@ -1,10 +1,9 @@
 const express = require("express");
-
-//const tarifRouter = require("./recipe/recipe-router");
-
+const router = require("./tarifler/tarif-router");
 const server = express();
 
 server.use(express.json());
-//server.use("/api/tarifler", tarifRouter);
+
+server.use("/api/tarifler", router);
 
 module.exports = server;
